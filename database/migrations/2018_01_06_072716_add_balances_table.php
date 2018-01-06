798c743e9ab6c0174d06be5aacaa8f712fd080f4
Martynas <martynas.andriuskevicius@hostinger.com>
2018-01-06 10:03:11 +0200

    Fix balances migration

--- database/migrations/2018_01_06_072716_add_balances_table.php
+++ database/migrations/2018_01_06_072716_add_balances_table.php
@@ -15,10 +15,9 @@ class AddBalancesTable extends Migration
     {
         Schema::create('balances', function (Blueprint $table) {
             $table->string('wallet', 50)->primary();
-            $table->decimal('ico_balance', 18, 18)->nullable();
-            $table->decimal('ico_ether', 18, 18)->nullable();
-            $table->decimal('balance', 18, 18)->nullable();
-            $table->decimal('balance_updated_at', 18, 18)->nullable();
+            $table->decimal('ico_balance', 32, 18)->nullable();
+            $table->decimal('ico_ether', 32, 18)->nullable();
+            $table->decimal('balance', 32, 18)->nullable();
             $table->timestamps();
         });
     }
